9eaf59ec44003950ab3837c48dba52bc7320c2d7
Sreeja-99 <sreejakaparthi1997@gmail.com>
2020-12-01 10:38:18 +0530

    This is commit by Y from US

--- src/test/java/MavenProject/ClassATest.java
+++ src/test/java/MavenProject/ClassATest.java
@@ -7,18 +7,21 @@ public class ClassATest {
 	public void Test1()
 	{
 		System.out.println("This is 1st testcase ");
+		System.out.println("This proj is imported from GitHub. This is 1st testcase and belongs to ClassA");
 	}
 	
 	@Test
 	public void Test2()
 	{
 		System.out.println("This is 2nd testcase ");
+		System.out.println("This proj is imported from GitHub. This is 2st testcase and belongs to ClassA");
 	}
 	
 	@Test
 	public void Test3()
 	{
 		System.out.println("This is 3rd testcase ");
+		System.out.println("This proj is imported from GitHub. This is 3rd testcase and belongs to ClassA");
 	}
 
 }
