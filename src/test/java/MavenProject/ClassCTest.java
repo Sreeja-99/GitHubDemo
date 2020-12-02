7107fca8139656361db2fd7e4a62bfa7b72388e8
Sreeja-99 <sreejakaparthi1997@gmail.com>
2020-12-02 15:41:01 +0530

    Branch code added by US Team

--- src/test/java/MavenProject/ClassCTest.java
+++ src/test/java/MavenProject/ClassCTest.java
@@ -7,18 +7,24 @@ public class ClassCTest{
 	public void Test7()
 	{
 		System.out.println("This is 7th testcase ");
+		System.out.println("This is imported from GitbHub");
+		System.out.println("Adding new testcase to reflect in branch by US guy");
 	}
 	
 	@Test
 	public void Test8()
 	{
 		System.out.println("This is 8th testcase ");
+		System.out.println("This is imported from GitbHub");
+		System.out.println("Adding new testcase to reflect in branch by US guy")
 	}
 	
 	@Test
 	public void Test9()
 	{
 		System.out.println("This is 9th testcase ");
+		System.out.println("This is imported from GitbHub");
+		System.out.println("Adding new testcase to reflect in branch by US guy")
 	}
 
 }
